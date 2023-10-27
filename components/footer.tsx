4a650f683b18c2dc847873cb3e4c268d729d6938
Chrisk1905 <Chrisk1905@gmail.com>
2023-10-26 22:43:25 -0700

    google map usedLoadScript

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,9 @@ import { useMemo } from "react"
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 
 export default function Footer(){
+    const { isLoaded } : {isLoaded:boolean} = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '', 
+    });
 
     return (
         <div>
