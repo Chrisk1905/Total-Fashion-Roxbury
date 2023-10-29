c7406aa2e5b2ca7fa38e8d22305c78b18f0f2124
Chrisk1905 <Chrisk1905@gmail.com>
2023-10-28 18:12:22 -0700

    Maps component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,25 +1,47 @@
+'use client';
+
 import { useMemo } from "react"
+
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 
 export default function Footer(){
-    const { isLoaded } : {isLoaded:boolean} = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '', 
-    });
 
     return (
         <div>
             <div>
                 <ul>
-                <li>Monday 9AM-6PM</li>
-                <li>Tuesday 9AM-6PM</li>
-                <li>Wednesday 9AM-6PM</li>
-                <li>Thursday 9AM-6PM</li>
-                <li>Friday 9AM-6PM</li>
-                <li>Saturday 9AM-4PM</li>
+                    <li>Monday 9AM-6PM</li>
+                    <li>Tuesday 9AM-6PM</li>
+                    <li>Wednesday 9AM-6PM</li>
+                    <li>Thursday 9AM-6PM</li>
+                    <li>Friday 9AM-6PM</li>
+                    <li>Saturday 9AM-4PM</li>
                 </ul>
-                <div id="map"></div>
-
             </div>
+            <Map/>
          </div>
+    )
+}
+
+
+function Map(){
+    const {isLoaded} = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '',
+    })    
+
+    if(!isLoaded){
+        return (
+            <div>Loading...</div>
+        )
+    }
+
+    return (
+        <GoogleMap 
+            zoom={10}
+            center={{ lat: 44, lng: -80}}
+            mapContainerClassName="map-container"
+        >
+
+        </GoogleMap>
     )
 }
