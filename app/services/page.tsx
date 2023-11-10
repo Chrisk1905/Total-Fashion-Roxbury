e916fc1e5efce1b529866fcd68eda3255f49697c
Chrisk1905 <Chrisk1905@gmail.com>
2023-11-09 21:47:22 -0800

    services card

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,14 +5,17 @@ export default function Page(){
     return (
         <>
             <div className={styles.service_container}>
-                <div>
+                <div className={styles.card}>
                     <h1>Dry Clean</h1>
+                    <p>We do Dry Cleaning. We'll take professional and timely care.</p>
                 </div>
-                <div>
-                    <h1>Alterations</h1>
+                <div className={styles.card}>
+                    <h1>Alterations & Repairs</h1>
+                    <p>We'll fix up your torn clothes, or alter for the perfect fit!</p>
                 </div>
-                <div>
+                <div className={styles.card}>
                     <h1>Wedding Dress</h1>
+                    <p>We have over a decade of wisdom and experiece in fitting wedding dresses.</p>
                 </div>
             </div>
        </>
