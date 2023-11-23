6749f6842ce6378a75237aaa9460f7f91960ad66
Chrisk1905 <Chrisk1905@gmail.com>
2023-11-22 22:22:28 -0800

    Advanced Marker and Info Window

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -5,13 +5,19 @@ import { useState } from 'react';
 import {APIProvider, Map, AdvancedMarker, Pin, InfoWindow} from '@vis.gl/react-google-maps';
 
 export default function TF_Map() {
-  const position = { lat:47.51759338378906, lng: -122.35620880126953 }
+  const position = { lat:47.51751829160094, lng:-122.35623009473451}
+  const [open, setOpen] = useState(false);
 
   return(
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
       <div className={styles.map_container}>
-        <Map zoom={13.5} center={position}>
-
+        <Map zoom={16} center={position} mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} > 
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin></Pin>
+          </AdvancedMarker>
+            {open && <InfoWindow position={position} onCloseClick = {() => setOpen(false)}>
+              <p>Total Fashion </p>
+            </InfoWindow>}
         </Map>
       </div>
     </APIProvider>
