052350636368db7541654a53ca6681df92c89808
Chrisk1905 <Chrisk1905@gmail.com>
2024-01-06 16:55:52 -0800

    hamburger menu updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,18 +23,13 @@ export default function RootLayout({
           <Link className="nav_element" href='./services'> Services </Link>
           <Link className="nav_element" href='./contact'> Contact </Link>
         </nav>
-        <input type="checkbox" id="hamburger-input" class="burger-shower" />
+        <input type="checkbox" id="hamburger-input" className="burger-shower" />
         <label id="hamburger-menu" for="hamburger-input">
           <nav id="sidebar-menu">
-            <h3>Menu</h3>
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Store</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </nav>
+            <Link className="nav_element" href='./'> Home </Link>
+            <Link className="nav_element" href='./services'> Services </Link>
+            <Link className="nav_element" href='./contact'> Contact </Link> 
+            </nav>
         </label>
         <div className="overlay"></div>
         {children}
