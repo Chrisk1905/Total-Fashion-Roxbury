358b9523127842f8826731ff107f53f6d6d379a3
Chrisk1905 <Chrisk1905@gmail.com>
2024-01-17 22:46:13 -0800

    adding Fontawesome bars icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,24 @@
+"use client"
+
 import Link from 'next/link'
 import Image from 'next/image'
 import Logo from 'public/Total Fashion-logos_transparent.png'
 import Footer from './components/footer'
 import './globals.css'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const [isLinksVisible, setLinksVisible] = useState(false);
 
-  
+  const myFunction = () => {
+    setLinksVisible((prevValue) => !prevValue);
+  };
   return (
     <html lang="en">
       <head>
@@ -24,8 +32,8 @@ export default function RootLayout({
           <Link className="nav_element" href='./'> Home </Link>
           <Link className="nav_element" href='./services'> Services </Link>
           <Link className="nav_element" href='./contact'> Contact </Link>
-          <Link className='hamburger-menu' >
-            <i></i>
+          <Link className='hamburger-menu' href="#" onClick={myFunction}>
+            <FontAwesomeIcon icon={faBars} />
           </Link>
         </nav>
         
