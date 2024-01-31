33c2220ad636e71bed7af9d34c83920b3a7bdba0
Chrisk1905 <Chrisk1905@gmail.com>
2024-01-31 11:53:36 -0800

    content: change in home page mission statement

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
           />
         <div className={styles.Hero_Content} >
           <p> Total Fashion Dry Cleaning and Alterations is a family bussiness owned and operated by expert seamstress and dressmaker Stephanie Kim.
-            We are proud to serve the White Center and the wider Seattle community. </p>
+            For over a deacde we have been proudly serving White Center and the wider Seattle community. </p>
           <p> At Total Fashion, our mission is to provide dependable alterations and dry cleaning services. 
             With a commitment to quality, precision, and care, we take pride in providing our clients 
             with elegance and a proper fit to their wardrobe.</p>
           <p> As a family business, we embrace the values of friendly service, integrity and fostering long-lasting 
-            relationships with our customers and community. </p>
+            relationships with our customers and community. Come on in for your dry cleaning and alterations needs!</p>
         </div>
       </div>
     </>
